a08a43755d1d23b1f00afc9d236afb9fbb84888f
vsheva <valera.sheva@gmail.com>
2023-02-01 23:15:51 -0500

    Add partials

--- app.js
+++ app.js
@@ -12,13 +12,17 @@ app.listen(3000);
 app.get('/', (req, res) => {
    //res.send("<h1>Hello Valera from the server</h1>");
    //res.sendFile('./views/index.html', {root: __dirname });
-   res.render("index.ejs");
+   const blogs = [{title:"Beautiful Life", snippet: "In this blog, I wanted to focus on the feeling that there's a real strength in the human spirit when you try to look for beauty in different situations."},
+      {title:"How Can We Protect the North Pacific Right Whale?", snippet: " The North Pacific right whale is one of the rarest of all large whale species, and one of the most endangered species on the planet.Despite its massive size (more than 50 feet long!), the North Pacific right whale feeds on tiny plankton by filtering hundreds of gallons of seawater in a single gulp to filter out the organisms with baleen plates."},
+      {title:"Climate Change", snippet: "Climate change blog articles about climate crisis, global warming, and our responsibility to climate change. Helping inspire and restore our environment."},
+   ];
+   res.render("index.ejs", {title:"Home", blogs});
 });
 
 app.get('/about', (req, res) => {
    //res.send("<h1>About Shevchenko page</h1>");
    //res.sendFile('./views/about.html', {root: __dirname });
-   res.render("about.ejs");
+   res.render("about.ejs", {title:"About"});
 
 });
 
@@ -30,7 +34,7 @@ app.get('/about', (req, res) => {
 })
  */
 app.get('/blogs/create', (req, res) => {
-   res.render("create.ejs");
+   res.render("create.ejs", {title:"Create a new Blog"});
 });
 
 
@@ -38,5 +42,5 @@ app.get('/blogs/create', (req, res) => {
 //404 page
 app.use((req, res)=>{
    //res.status(404).sendFile('./views/404.html', {root: __dirname})
-   res.status(404).render("404.ejs")
+   res.status(404).render("404.ejs", {title: "404"})
 })
