6ea7f807991b6bf6207f972f26f9d7671f915151
vsheva <valera.sheva@gmail.com>
2023-02-03 00:34:49 -0500

    Getting and saving data

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
 const mongoose = require('mongoose');
+const Blog = require('./models/blog.js');
 
 const app = express();
 
@@ -23,34 +24,44 @@ app.use(express.static('public'));
 
 app.use(morgan('dev'));
 
+//routes
 app.get('/', (req, res) => {
-  //res.send("<h1>Hello Valera from the server</h1>");
-  //res.sendFile('./views/index.html', {root: __dirname });
-  const blogs = [
-    {
-      title: 'Beautiful Life',
-      snippet:
-        "In this blog, I wanted to focus on the feeling that there's a real strength in the human spirit when you try to look for beauty in different situations.",
-    },
-    {
-      title: 'How Can We Protect the North Pacific Right Whale?',
-      snippet:
-        ' The North Pacific right whale is one of the rarest of all large whale species, and one of the most endangered species on the planet.Despite its massive size (more than 50 feet long!), the North Pacific right whale feeds on tiny plankton by filtering hundreds of gallons of seawater in a single gulp to filter out the organisms with baleen plates.',
-    },
-    {
-      title: 'Climate Change',
-      snippet:
-        'Climate change blog articles about climate crisis, global warming, and our responsibility to climate change. Helping inspire and restore our environment.',
-    },
-  ];
-  res.render('index.ejs', { title: 'Home', blogs });
+  res.redirect('/blogs')
+  // const blogs = [
+  //   {
+  //     title: 'Beautiful Life',
+  //     snippet:
+  //       "In this blog, I wanted to focus on the feeling that there's a real strength in the human spirit when you try to look for beauty in different situations.",
+  //   },
+  //   {
+  //     title: 'How Can We Protect the North Pacific Right Whale?',
+  //     snippet:
+  //       ' The North Pacific right whale is one of the rarest of all large whale species, and one of the most endangered species on the planet.Despite its massive size (more than 50 feet long!), the North Pacific right whale feeds on tiny plankton by filtering hundreds of gallons of seawater in a single gulp to filter out the organisms with baleen plates.',
+  //   },
+  //   {
+  //     title: 'Climate Change',
+  //     snippet:
+  //       'Climate change blog articles about climate crisis, global warming, and our responsibility to climate change. Helping inspire and restore our environment.',
+  //   },
+  // ];
+  // res.render('index.ejs', { title: 'Home', blogs });
 });
 
 app.get('/about', (req, res) => {
-  //res.send("<h1>About Shevchenko page</h1>");
-  //res.sendFile('./views/about.html', {root: __dirname });
   res.render('about.ejs', { title: 'About' });
 });
+
+
+//blog routes
+app.get('/blogs', (req, res)=>{
+  Blog.find().sort({createdAt: -1})
+      .then((result)=>{
+        res.render('index.ejs', { title: 'All blogs', blogs:result });
+      })
+      .catch((err)=>console.log(err))
+})
+
+
 
 app.get('/blogs/create', (req, res) => {
   res.render('create.ejs', { title: 'Create a new Blog' });
@@ -61,3 +72,43 @@ app.use((req, res) => {
   //res.status(404).sendFile('./views/404.html', {root: __dirname})
   res.status(404).render('404.ejs', { title: '404' });
 });
+
+
+
+
+
+/**
+ //mongoose and mongo sandbox routes
+
+ app.get('/add-blog', (req, res)=>{
+  const blog= new Blog({
+    title: "new blog 2",
+    snippet: "about  new blog",
+    body: "more information about new blog"
+  });
+
+blog.save()
+    .then((result)=>{
+      res.send(result)
+    })
+    .catch((err)=>{console.log(err)});
+})
+
+ //all-blogs
+ app.get('/all-blogs', (req, res)=>{
+  Blog.find()
+      .then((result)=>{
+        res.send(result);
+      })
+      .catch(err=>{console.log(err)});
+});
+
+ //single-blog
+ app.get('/single-blog', (req, res)=>{
+  Blog.findById("63dc8f78146aaa7b6372f0b8")
+      .then((result)=>{
+        res.send(result);
+      })
+      .catch(err=>{console.log(err)});
+})
+ */
