01a84b1881c3eedcb5584e11a98f6ba0257dcce2
vsheva <valera.sheva@gmail.com>
2023-02-04 23:00:25 -0500

    complete

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const morgan = require('morgan');
 const mongoose = require('mongoose');
 
-const blogRoutes = require('./routes/blogRoutes.js')
+const blogRoutes = require('./routes/blogRoutes.js');
 
 const app = express();
 
@@ -17,7 +17,7 @@ mongoose
   .then(res => app.listen(3000))
   .catch(err => console.log(err));
 
-//register view engine
+//view engine
 app.set('view engine', 'ejs');
 
 //middleware & static files
@@ -39,66 +39,10 @@ app.get('/about', (req, res) => {
 });
 
 //blog routes
-app.use(blogRoutes)
-
+app.use(blogRoutes);
 
 //404 page
 app.use((req, res) => {
   //res.status(404).sendFile('./views/404.html', {root: __dirname})
   res.status(404).render('404.ejs', { title: '404' });
 });
-
-// const blogs = [
-//   {
-//     title: 'Beautiful Life',
-//     snippet:
-//       "In this blog, I wanted to focus on the feeling that there's a real strength in the human spirit when you try to look for beauty in different situations.",
-//   },
-//   {
-//     title: 'How Can We Protect the North Pacific Right Whale?',
-//     snippet:
-//       ' The North Pacific right whale is one of the rarest of all large whale species, and one of the most endangered species on the planet.Despite its massive size (more than 50 feet long!), the North Pacific right whale feeds on tiny plankton by filtering hundreds of gallons of seawater in a single gulp to filter out the organisms with baleen plates.',
-//   },
-//   {
-//     title: 'Climate Change',
-//     snippet:
-//       'Climate change blog articles about climate crisis, global warming, and our responsibility to climate change. Helping inspire and restore our environment.',
-//   },
-// ];
-// res.render('index.ejs', { title: 'Home', blogs });
-
-/**
- //mongoose and mongo sandbox routes
-
- app.get('/add-blog', (req, res)=>{
-  const blog= new Blog({
-    title: "new blog 2",
-    snippet: "about  new blog",
-    body: "more information about new blog"
-  });
-
-blog.save()
-    .then((result)=>{
-      res.send(result)
-    })
-    .catch((err)=>{console.log(err)});
-})
-
- //all-blogs
- app.get('/all-blogs', (req, res)=>{
-  Blog.find()
-      .then((result)=>{
-        res.send(result);
-      })
-      .catch(err=>{console.log(err)});
-});
-
- //single-blog
- app.get('/single-blog', (req, res)=>{
-  Blog.findById("63dc8f78146aaa7b6372f0b8")
-      .then((result)=>{
-        res.send(result);
-      })
-      .catch(err=>{console.log(err)});
-})
- */
--- controllers/blogController.js
+++ controllers/blogController.js
@@ -15,7 +15,9 @@ const blog_details = (req, res) => {
     .then(result => {
       res.render('details', { blog: result, title: 'Blog Details' });
     })
-    .catch(err => console.log(err));
+    .catch(err => {
+      res.status(404).render('404', { title: 'Weblog not found' });
+    });
 };
 
 const blog_create_get = (req, res) => {
